c1db93b4ae6eb5ebb2db9da4fc9a1b7ffd6e65f1
laksh95 <goel.laksh95@gmail.com>
2017-01-30 12:10:05 +0530

    reg complete

---
+++ js/country.js
@@ -0,0 +1,20 @@
+$(function(){
+	var all_countries; 	
+	$.ajax({
+		url:"https://restcountries.eu/rest/v1/all",
+		success:function(response){
+			all_countries = response;
+			// console.log(all_countries);
+			countries=[]; 
+			$.each(all_countries,function(index,value){
+			countries.push(all_countries[index].name);
+			});
+			var country = $("#country");
+			var list = "" ;
+			$.each(countries,function(index,value){
+				list+="<option>"+value+"</option>";
+			});
+			country.append(list);
+		}
+	});
+});
